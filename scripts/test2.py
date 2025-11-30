0eed78654d5ec8411eca31c24896f3b77cc0394e
SabineRiffel <Sabine.Riffel@Student.HTW-Berlin.de>
2025-11-30 15:52:42 +0100

    Stop tracking test2.py

--- scripts/test2.py
+++
@@ -1,8 +0,0 @@
-
-
-import pandas as pd
-
-df = pd.read_csv("/Users/sabine/PycharmProjects/Project-Trading/data/AAPL_news.csv")
-
-df.describe(include="object").to_csv("/Users/sabine/PycharmProjects/Project-Trading/data/descrie.csv", index=True)
-#df.describe().to_csv("/Users/sabine/PycharmProjects/Project-Trading/data/descrie.csv", index=True)
